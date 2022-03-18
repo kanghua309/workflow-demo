0ed220b35ee8cb87479678f6908f1aafb365ec73
kanghua309 <kanghua151@msn.com>
2022-03-18 10:45:32 +0800

    test trigger 2

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreet(t *testing.T) {
 	result := Greet()
-	if result != "Hello GitHub Actions 2" {
+	if result != "Hello GitHub Actions 3" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub actions!", result)
 	}
 }
